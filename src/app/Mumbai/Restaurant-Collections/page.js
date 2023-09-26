a1521aea69b20fd51bd8ab4602e2ae0519f05d5a
pverma2905 <pverma2905@gmail.com>
2023-09-26 12:38:36 +0530

    first

---
+++ src/app/Mumbai/Restaurant-Collections/page.js
@@ -0,0 +1,12 @@
+//import area
+import React from 'react'
+
+// definition area
+ function page() {
+  return (
+    <div>Restaurant-Collections</div>
+  )
+}
+
+// export area
+export default page
---
+++ src/components/A.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import B from './B'
+
+export default function A() {
+  return (
+    <>
+    <div>A Component</div>
+
+        <B/>
+    </>
+  )
+}
